9d7ba91554a65060fccb344f2bc7a0dd72c2947c
Art <art@art.art>
2023-05-28 13:21:25 +0300

    Clean up

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,25 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { CatsController } from './cats/cats.controller';
-import { UsersController } from './users/users.controller';
-import { PostsController } from './posts/posts.controller';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {AuthModule} from './auth/auth.module';
+import {UsersController} from './users/users.controller';
+import {PostsController} from './posts/posts.controller';
 import * as SuperTokensConfig from './config';
 import {PostService} from "./posts/post.service";
 import {UserService} from "./users/user.service";
 import {PrismaService} from "./prisma.service";
 
 @Module({
-  imports: [
-    AuthModule.forRoot({
-      // try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
-      connectionURI: SuperTokensConfig.connectionUri,
-      // apiKey: "IF YOU HAVE AN API KEY FOR THE CORE, ADD IT HERE",
-      appInfo: SuperTokensConfig.appInfo,
-    }),
-  ],
-  controllers: [AppController, CatsController, UsersController, PostsController],
-  providers: [AppService, PrismaService, UserService, PostService],
+    imports: [
+        AuthModule.forRoot({
+            // try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
+            connectionURI: SuperTokensConfig.connectionUri,
+            // apiKey: "IF YOU HAVE AN API KEY FOR THE CORE, ADD IT HERE",
+            appInfo: SuperTokensConfig.appInfo,
+        }),
+    ],
+    controllers: [AppController, UsersController, PostsController],
+    providers: [AppService, PrismaService, UserService, PostService],
 })
-export class AppModule {}
+export class AppModule {
+}
